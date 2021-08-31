642f3b37f9d011eae3a3b099d396ba76f52c2d4d
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-08-31 22:53:21 +0200

    correzione pari e dispari, consegna misinterpretata

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -11,17 +11,62 @@ Consigli del giorno
 Scriviamo sempre in italiano i passaggi che vogliamo fare
 Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.*/
 
-/*
-let numeroRandomUtente = parseInt(prompt('Scegli pari o dispari e inserisci un numero da 1 a 5'));
 
-while (numeroRandomUtente > 5 || Number.isNaN(numeroRandomUtente)) {
+
+/*
+
+let parolaUtente = prompt("Inserisci una parola");
+
+function creazioneStringaAlContrario(stringa) { //stringa capovolta
+
+    let stringaCapovolta = stringa.split("").reverse().join("");
+
+    return stringaCapovolta
+}
+ 
+//console.log(creazioneStringaAlContrario(parolaUtente)); //stampa stringa capovolta
+
+if (parolaUtente === creazioneStringaAlContrario(parolaUtente)) { //attenzione, capovolgendo questa condizione non funziona
+                                                            
+    console.log('La parola è palindroma');
+    } else {
+    console.log ('La parola NON è palindroma');
+    }
+
+
+    
+*/
+
+
+
+let pariOppureDispari = prompt('Scegli pari o dispari');
+
+console.log('Utente ha scelto: ' + pariOppureDispari) //stampa scelta pari o dispari dell'utente
+
+let numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 5'));
+
+console.log('Il numero scelto da utente è: ' + numeroUtente); //stampa input utente numero
+
+while (numeroUtente > 5 || Number.isNaN(numeroUtente)) {
     alert('Devi inserire un numero compreso tra 1 e 5!');
-    numeroRandomUtente = prompt('Scegli pari o dispari e inserisci un numero da 1 a 5');
+    numeroRandomUtente = prompt('Inserisci un numero da 1 a 5');
 }
 
-let numeroRandomComputer = Math.floor(Math.random() * 5) + 1; //vale come "funzione"?
+//let numeroRandomComputer = Math.floor(Math.random() * 5) + 1; //è pur sempre una funzione già di per sè...?
 
-let sommaNumeri = numeroRandomUtente + numeroRandomComputer;
+function numeroRandomPc (min, max) {
+    min = Math.ceil(min);           // prende il massimo del numero minimo dato (arrotondamento per eccesso)
+    max = Math.floor(max);          // prende il minimo del numero massimo dato (arrotondamento per difetto)
+    return Math.floor(Math.random() * (max - min + 1)) + min; // calcola un numero randomico nel suo intervallo
+} 
+
+let numeroRandomComputer = numeroRandomPc(1, 5);
+
+console.log('Il numero randomico del computer è: ' + numeroRandomComputer); 
+
+let sommaNumeri = numeroUtente + numeroRandomComputer; //somma input utente e numero random computer
+
+console.log('La somma dei due numeri randomici è: ' + sommaNumeri); //stampa numero random del computer
 
 function testPariODispari (numero) {
     if (numero % 2 == 0) {
@@ -31,115 +76,36 @@ function testPariODispari (numero) {
     }
 }
 
-console.log('Il numero scelto da utente è: ' + testPariODispari(numeroRandomUtente));
+if (testPariODispari(sommaNumeri) === pariOppureDispari ) {
+   
+    console.log('Ha vinto utente')
+    } else {
+        console.log('Ha vinto il computer')
+    } 
 
-console.log('Il numero randomico del computer è: ' + testPariODispari(numeroRandomComputer));
 
-console.log('La somma dei due numeri randomici è: ' + testPariODispari(sommaNumeri));
 
-if ((testPariODispari(numeroRandomUtente) != testPariODispari(sommaNumeri) &&
+
+//consegna non interpretata bene; lascio comunque passaggi dove credevo servisse far vincere in base al numero scelto
+//(in quanto pari o dispari) rapportato alla somma (pari o dispari)
+    
+/*if ((testPariODispari(numeroRandomUtente) != testPariODispari(sommaNumeri) &&
      testPariODispari(numeroRandomComputer) != testPariODispari(sommaNumeri))) {
     console.log('Avete perso entrambi');
 } else if (testPariODispari(numeroRandomUtente) === testPariODispari(numeroRandomComputer)) {
     console.log('Ha vinto lo sport!');
-}
+}*/
 
 
 
-if (testPariODispari(sommaNumeri) === 'pari' ) {
-    if (numeroRandomUtente % 2 == 0) {
-        console.log('Ha vinto utente')
-    } else if (numeroRandomComputer % 2 == 1) {
-        console.log('Ha vinto il computer')
-    } 
-
-}
-
-if (testPariODispari(sommaNumeri) === 'dispari' ) {
+/*if (testPariODispari(sommaNumeri) === 'dispari' ) {
     
     if (numeroRandomUtente % 2 == 1) {
         console.log('Ha vinto utente')
     } else if (numeroRandomComputer % 2 == 1) {
         console.log('Ha vinto il computer')
     }
-}
+}*/
 
-*/
-
-let parolaUtente = prompt("Inserisci una parola");
-
-function creazioneStringaAlContrario(stringa) { //capovolge la parola
-
-    let splitString = stringa.split("").reverse().join("");
-
-    return splitString
-}
- 
-//console.log(creazioneStringaAlContrario(parolaUtente)); //ha capovolto la parola
-
-if (parolaUtente === creazioneStringaAlContrario(parolaUtente)) { //attenzione, se si mette prima l'estratto
-    //della funzione si ferma sempre alla prima condizione
-    console.log('La parola è palindroma');
-    } else {
-    console.log ('La parola NON è palindroma');
-    }
-
-/*function isParolaPariODispari(parola) { //gli sto facendo verificare se la parola è pari o dispari
-    if (parola.length % 2 == 1) {
-        return 'dispari'
-    }
-} */
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-
-//COPIATO E INCOLLATO DA INTERNET!!! DA RIFARE!!!
-
-function check_Palindrome(str_entry){
-    // Change the string into lower case and remove  all non-alphanumeric characters
-       var cstr = str_entry.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
-        var ccount = 0;
-    // Check whether the string is empty or not
-        if(cstr==="") {
-            console.log("Devi inserire una parola!!!");
-            return false;
-        }
-    // Check if the length of the string is even or odd 
-        if ((cstr.length) % 2 === 0) {
-            ccount = (cstr.length) / 2;
-        } else {
-    // If the length of the string is 1 then it becomes a palindrome
-            if (cstr.length === 1) {
-                console.log("La parola è un palindromo");
-                return true;
-            } else {
-    // If the length of the string is odd ignore middle character
-                ccount = (cstr.length - 1) / 2;
-            }
-        }
-    // Loop through to check the first character to the last character and then move next
-        for (var x = 0; x < ccount; x++) {
-    // Compare characters and drop them if they do not match 
-            if (cstr[x] != cstr.slice(-1-x)[0]) {
-                console.log("La parola non è un palindromo");
-                return false;
-            }
-        }
-        console.log("La parola è un palindromo");
-        return true;
-    }
-    //check_Palindrome(parolaUtente)*/
 
 
